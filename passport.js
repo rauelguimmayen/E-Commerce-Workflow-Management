1168f9e08d4ffd17e91e1abb42070f0a42b51038
rauelguimmayen <rauelguimmayen@gmail.com>
2026-06-18 19:25:49 +0800

    updated google oauth

--- passport.js
+++ passport.js
@@ -1,6 +1,7 @@
 // passport.js
 const User = require('./models/User'); // adjust path to your actual model
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
